c7b53bb4a96146bc7845069628e2b73497d1a261
dllo <dllo@wangjiandeMac-mini-4.local>
2016-07-18 18:54:28 +0800

    no message

--- app/src/main/java/com/lanou/evernote/loginandregister/LoginAndRegisterModel.java
+++ app/src/main/java/com/lanou/evernote/loginandregister/LoginAndRegisterModel.java
@@ -1,12 +1,19 @@
 package com.lanou.evernote.loginandregister;
 
+import android.widget.Toast;
+
+import com.lanou.evernote.base.MyApplication;
+
 import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.listener.SaveListener;
 
 /**
  * Created by dllo on 16/7/18.
  */
 public class LoginAndRegisterModel implements LoginAndRegistterContract.Model {
     private LoginAndRegistterContract.Presenter presenter;
+
+
 
 
 
@@ -18,12 +25,45 @@ public class LoginAndRegisterModel implements LoginAndRegistterContract.Model {
 
     @Override
     public void checkLoginFoNet(String userName, String password) {
-    //    BmobUser bmobUser = BmobUser.getCurrentUser()
+        BmobUser bmobUser = BmobUser.getCurrentUser(MyApplication.getContext());
+        bmobUser = new BmobUser();
+        bmobUser.setUsername(userName);
+        bmobUser.setPassword(password);
+        bmobUser.login(MyApplication.getContext(), new SaveListener() {
+            @Override
+            public void onSuccess() {
+               presenter.loginSuccess();
+            }
+
+            @Override
+            public void onFailure(int i, String s) {
+                presenter.loginError("登录失败");
+            }
+        });
+
+
+
 
     }
 
     @Override
     public void checkRegisterFoNet(String userName, String password) {
+        BmobUser bmobUser = BmobUser.getCurrentUser(MyApplication.getContext());
+        bmobUser = new BmobUser();
+        bmobUser.setUsername(userName);
+        bmobUser.setPassword(password);
+        bmobUser.signUp(MyApplication.getContext(), new SaveListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int i, String s) {
+                presenter.loginError("该账号已经存在");
+            }
+        });
+
 
     }
 
