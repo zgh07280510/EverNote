9dc89cf28e2d0fcdd27d76d52e39edb1c102c9e5
邹国华 <zgh07280510@126.com>
2016-07-18 19:20:07 +0800

    no message

--- app/src/main/java/com/lanou/evernote/base/BaseActivity.java
+++ app/src/main/java/com/lanou/evernote/base/BaseActivity.java
@@ -1,8 +1,10 @@
 package com.lanou.evernote.base;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by zouguohua on 16/7/18.
@@ -13,10 +15,13 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayout());
+        initview();
         initData();
 
 
     }
+
+    protected abstract void initview( );
 
     public abstract int setLayout();
 
