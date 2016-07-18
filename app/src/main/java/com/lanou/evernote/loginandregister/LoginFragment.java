3527a1947d5cb1dab4976774809864346f26ad50
dllo <331941944@qq.com>
2016-07-18 19:50:50 +0800

    no message

---
+++ app/src/main/java/com/lanou/evernote/loginandregister/LoginFragment.java
@@ -0,0 +1,26 @@
+package com.lanou.evernote.loginandregister;
+
+import android.view.View;
+
+import com.lanou.evernote.R;
+import com.lanou.evernote.base.BaseFragment;
+
+/**
+ * Created by dllo on 16/7/18.
+ */
+public class LoginFragment extends BaseFragment {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected int setlayout() {
+        return R.layout.fragment_login;
+    }
+}
