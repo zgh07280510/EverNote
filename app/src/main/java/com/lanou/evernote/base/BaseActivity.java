3b85efbf9a4856fa55c4c4c9c6894a780e4e8ce9
dllo <331941944@qq.com>
2016-07-19 19:04:07 +0800

    no message

--- app/src/main/java/com/lanou/evernote/base/BaseActivity.java
+++ app/src/main/java/com/lanou/evernote/base/BaseActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by zouguohua on 16/7/18.
  */
@@ -15,6 +17,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(setLayout());
+        ButterKnife.bind(this);
         initView();
         initData();
 
--- app/src/main/java/com/lanou/evernote/search/SearchAty.java
+++ app/src/main/java/com/lanou/evernote/search/SearchAty.java
@@ -1,14 +1,32 @@
 package com.lanou.evernote.search;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.lanou.evernote.R;
 import com.lanou.evernote.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by zouguohua on 16/7/19.
  */
 public class SearchAty extends BaseActivity {
+
+
+    @BindView(R.id.btn_search_back)
+    Button btnSearchBack;
+    @BindView(R.id.et_search)
+    EditText etSearch;
+    @BindView(R.id.search_listView)
+    ListView searchListView;
+
     @Override
     public int setLayout() {
-        return 0;
+
+        return R.layout.activity_search;
     }
 
     @Override
@@ -19,5 +37,13 @@ public class SearchAty extends BaseActivity {
     @Override
     protected void initData() {
 
+    }
+
+
+
+
+    @OnClick(R.id.btn_search_back)
+    public void onClick() {
+       finish();
     }
 }
