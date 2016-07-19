2530fdc1dc76a356a3caeeef290f6402fae08b82
邹国华 <zgh07280510@126.com>
2016-07-19 15:32:00 +0800

    创建搜索Aty

---
+++ app/src/main/java/com/lanou/evernote/search/SearchAty.java
@@ -0,0 +1,23 @@
+package com.lanou.evernote.search;
+
+import com.lanou.evernote.base.BaseActivity;
+
+/**
+ * Created by zouguohua on 16/7/19.
+ */
+public class SearchAty extends BaseActivity {
+    @Override
+    public int setLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
