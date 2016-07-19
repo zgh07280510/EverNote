5bed866cd3e2be2a6808cc343298d736667d7acb
邹国华 <zgh07280510@126.com>
2016-07-19 15:23:52 +0800

    创建主页

---
+++ app/src/main/java/com/lanou/evernote/homepage/HomepageAty.java
@@ -0,0 +1,23 @@
+package com.lanou.evernote.homepage;
+
+import com.lanou.evernote.base.BaseActivity;
+
+/**
+ * Created by zouguohua on 16/7/19.
+ */
+public class HomepageAty extends BaseActivity {
+    @Override
+    public int setLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
