6c9650108a4abc1e929c92f488502efee91f403b
邹国华 <zgh07280510@126.com>
2016-07-20 21:12:39 +0800

    相机Activity

---
+++ app/src/main/java/com/lanou/evernote/camera/CameraAty.java
@@ -0,0 +1,23 @@
+package com.lanou.evernote.camera;
+
+import com.lanou.evernote.base.BaseActivity;
+
+/**
+ * Created by zouguohua on 16/7/20.
+ */
+public class CameraAty extends BaseActivity  {
+    @Override
+    public int setLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
