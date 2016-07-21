112785380b26e9a422e944cbd06eee43c9ee6966
邹国华 <zgh07280510@126.com>
2016-07-21 10:03:43 +0800

    no message

--- app/src/main/java/com/lanou/evernote/camera/CameraAty.java
+++ app/src/main/java/com/lanou/evernote/camera/CameraAty.java
@@ -1,5 +1,8 @@
 package com.lanou.evernote.camera;
 
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+
 import com.lanou.evernote.R;
 import com.lanou.evernote.base.BaseActivity;
 
@@ -7,6 +10,9 @@ import com.lanou.evernote.base.BaseActivity;
  * Created by zouguohua on 16/7/20.
  */
 public class CameraAty extends BaseActivity {
+    private ImageView attachIv;//曲别针图片按钮
+    private PopupWindow popupWindow;
+
     @Override
     public int setLayout() {
         return R.layout.activity_camera;
@@ -14,11 +20,14 @@ public class CameraAty extends BaseActivity {
 
     @Override
     protected void initView() {
+        attachIv = (ImageView) findViewById(R.id.attach_enabled);
 
     }
 
     @Override
     protected void initData() {
+
+        popupWindow = new PopupWindow(this);
 
     }
 }
