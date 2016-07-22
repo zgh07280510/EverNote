09a3f6b49ced5ac55e02100c704499b372a2ec7d
dllo <331941944@qq.com>
2016-07-22 21:30:48 +0800

    no message

---
+++ app/src/main/java/com/lanou/evernote/attachment/AttachmentActivity.java
@@ -0,0 +1,23 @@
+package com.lanou.evernote.attachment;
+
+import com.lanou.evernote.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/7/22.
+ */
+public class AttachmentActivity extends BaseActivity {
+    @Override
+    public int setLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
--- app/src/main/java/com/lanou/evernote/search/SearchAty.java
+++ app/src/main/java/com/lanou/evernote/search/SearchAty.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
@@ -24,6 +23,7 @@ import com.lanou.evernote.base.ViewHolder;
 import com.lanou.evernote.tools.DisplayUtil;
 import com.lanou.evernote.tools.LocalData;
 import com.lanou.evernote.tools.SingleLiteOrm;
+import com.litesuits.orm.db.assit.QueryBuilder;
 
 import java.util.ArrayList;
 
@@ -38,11 +38,10 @@ public class SearchAty extends BaseActivity implements View.OnClickListener {
     private LinearLayout llGreen;
     private PopupWindow popupWindow;
     private ArrayList<LocalData> searchHistory;
-    private String svContent;
     private LocalData localData;
-    private String etContent;
     private TextView tvFactor;
     private View view;
+    private ListViewCommonAdapter adapter;
 
     @Override
     public int setLayout() {
@@ -59,6 +58,7 @@ public class SearchAty extends BaseActivity implements View.OnClickListener {
         btnAccurateSearch = (Button) findViewById(R.id.btn_accurate_search);
         llGreen = (LinearLayout) findViewById(R.id.ll_search_green);
         popupWindow = new PopupWindow();
+
     }
 
     @Override
@@ -66,11 +66,17 @@ public class SearchAty extends BaseActivity implements View.OnClickListener {
         view = LayoutInflater.from(this).inflate(R.layout.note_popupwindow, null);
         tvFactor = (TextView) view.findViewById(R.id.tv_factor_pop);
 
-        localData = new LocalData();
         searchHistory = new ArrayList<>();
-        for (int i = 0; i < searchHistory.size(); i++) {
-            searchHistory.add(localData);
-        }
+        searchHistory = SingleLiteOrm.getSingleLiteOrm().getLiteOrm().query(LocalData.class);
+
+        adapter = new ListViewCommonAdapter<LocalData>(this, searchHistory, R.layout.history_item_list) {
+            @Override
+            public void convert(ViewHolder holder, LocalData localData) {
+                holder.setText(R.id.tv_history, localData.getEtContent());
+            }
+        };
+
+
         searchRecodeListView.setTextFilterEnabled(true);
         svSearch.setIconifiedByDefault(false);
         svSearch.setQueryHint("搜索笔记");
@@ -80,40 +86,45 @@ public class SearchAty extends BaseActivity implements View.OnClickListener {
         btnBack.setOnClickListener(this);
         svSearch.setOnClickListener(this);
 
-        searchRecodeListView.setAdapter(new ListViewCommonAdapter<LocalData>(this, searchHistory, R.layout.history_item_list) {
-
-            @Override
-            public void convert(ViewHolder holder, LocalData localData) {
-              holder.setText(R.id.tv_history,localData.getEtContent());
-            }
-
-
-        });
         svSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
             @Override
             public boolean onQueryTextSubmit(String query) {
                 Toast.makeText(SearchAty.this, "您的选择是" + query, Toast.LENGTH_SHORT).show();
-                return false;
+                localData = new LocalData();
+                localData.setEtContent(query);
+                Log.d("SearchAty", query);
+
+                //查询数据库里是否有这条记录
+                QueryBuilder<LocalData> queryBuilder = new QueryBuilder<>(LocalData.class);
+                queryBuilder.whereEquals("etContent", localData.getEtContent());
+                if (SingleLiteOrm.getSingleLiteOrm().getLiteOrm().query(queryBuilder).size() == 0) {
+                    SingleLiteOrm.getSingleLiteOrm().getLiteOrm().insert(localData);
+                    searchHistory = SingleLiteOrm.getSingleLiteOrm().getLiteOrm().query(LocalData.class);
+                    adapter.addSingleData(searchHistory.get(searchHistory.size() - 1), searchHistory.size() + 1);
+                }
+
+
+
+
+                return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
                 if (TextUtils.isEmpty(newText)) {
                     searchRecodeListView.clearTextFilter();
+                    btnAccurateSearch.setVisibility(View.VISIBLE);
                 } else {
                     searchRecodeListView.setFilterText(newText);
-//                    etContent = newText;
+                    btnAccurateSearch.setVisibility(View.GONE);
 
-                    localData.setEtContent(newText);
-                    SingleLiteOrm.getSingleLiteOrm().getLiteOrm().insert(localData);
-                    Log.d("SearchAty", newText);
-                    searchHistory = SingleLiteOrm.getSingleLiteOrm().getLiteOrm().query(LocalData.class);
-                    Log.d("SearchAty", "searchHistory:" + searchHistory);
                 }
                 return true;
             }
         });
+
+        searchRecodeListView.setAdapter(adapter);
     }
 
     @Override
